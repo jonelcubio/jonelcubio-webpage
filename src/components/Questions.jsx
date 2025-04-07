b387ea83c26f5b565bc37d22d9f2fed19f87115d
mynameisjonelledev <jonelcubio021@gmail.com>
2025-04-07 14:54:38 +0800

    Completed Questions component (Kahit katatapos lang atakihin ng anxiety)

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -3,13 +3,18 @@ export function Questions() {
     <section className="question-container">
       <div className="question-title">Question 🙋🏻</div>
 
+      <div className="question-questions">Got any question? Don't hesistate to contact me!</div>
+
       <div className="question-pic-name">
         <div className="question-pic-con">
-          <img src="" alt="" className="question.pic-img" />
+          <img src="https://i.postimg.cc/pLPd88nc/IMG20250406090439.jpg" className="question-pic-img" />
         </div>
 
-        <div className="question-name"></div>
-        <div className="question email"></div>
+        <div className="question-name-email">
+        <div className="question-name">Jonel Cubio</div>
+        <a href="mailto:jonelcubio021@gmail.com"><div className="question-email">jonelcubio021@gmail.com</div></a>
+        </div>
+        
       </div>
     </section>
   );
